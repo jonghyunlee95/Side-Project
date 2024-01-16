1a46e2230cb66c590cad4b67d3c58ecb55c1ae91
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2024-01-16 17:46:07 +0900

    chore: prettier, eslint 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'react',
+    'jsx-a11y',
+    'react-hooks',
+    'prettier',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
   },
-}
+};
